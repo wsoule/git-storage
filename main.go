7cf6c8ba4e76f562b4347f610907431f901eb425
Wyat Soule <wsoule679@gmail.com>
2026-02-26 12:06:01 -0500

    feat: wire up main to start the server

--- main.go
+++ main.go
@@ -1,2 +1,31 @@
-package git
+package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"git.wyat.me/git-storage/server"
+)
+
+func main() {
+	repoRoot := os.Getenv("REPO_ROOT")
+	if repoRoot == "" {
+		repoRoot = "./repos"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	srv, err := server.New(repoRoot)
+	if err != nil {
+		log.Fatalf("failed to create server: %v", err)
+	}
+
+	log.Printf("listening on :%s, repos at %s", port, repoRoot)
+	if error := http.ListenAndServe(":"+port, srv.Handler()); error != nil {
+		log.Fatalf("failed to start server: %v", error)
+	}
+}
